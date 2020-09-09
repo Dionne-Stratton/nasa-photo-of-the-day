c5a48e94e399c585c19e41c0ab6ceebfe606d78d
Dionne Stratton <mylife33@hotmail.com>
2020-09-09 18:07:48 -0500

    deleted commented out redundancy

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import NasaData from "./Components/NasaData";
 // import axios from "axios";
 
 function App() {
-  // const [nasaInfo, setnasaInfo] = useState([]);
-  // useEffect(() => {
-  //   axios
-  //   .get("https://api.nasa.gov/planetary/apod?api_key=rDLuS1OxbD26VJPygpmoRhTKPA0l6bb4sIHGGQJi&date=2020-08-08")
-  //   //api data : date,explanation,hdurl,media_type,service_verison,title,url
-  //   .then(res =>{ 
-  //     console.log("res data :", res);
-  //   })
-  //   .catch(err => { 
-  //     console.log("err msg : ", err);
-  //   });
-  // })
+ 
   return (
     <div className="App">
       <NasaData />
