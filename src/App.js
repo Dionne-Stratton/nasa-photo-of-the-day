36f164354819e7d794ac4dd0a13d4df67801064a
Dionne Stratton <mylife33@hotmail.com>
2020-09-09 17:41:45 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import "./App.css";
+import NasaData from "./Components/NasaData";
+// import axios from "axios";
 
 function App() {
+  // const [nasaInfo, setnasaInfo] = useState([]);
+  // useEffect(() => {
+  //   axios
+  //   .get("https://api.nasa.gov/planetary/apod?api_key=rDLuS1OxbD26VJPygpmoRhTKPA0l6bb4sIHGGQJi&date=2020-08-08")
+  //   //api data : date,explanation,hdurl,media_type,service_verison,title,url
+  //   .then(res =>{ 
+  //     console.log("res data :", res);
+  //   })
+  //   .catch(err => { 
+  //     console.log("err msg : ", err);
+  //   });
+  // })
   return (
     <div className="App">
+      <NasaData />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
@@ -11,5 +26,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
